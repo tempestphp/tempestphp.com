280b1faa69c29426ef141ea4c70f0297dc26be47
brendt <brent.roose@gmail.com>
2026-07-29 08:57:25 +0200

    wip

--- src/Web/Blog/Author.php
+++ src/Web/Blog/Author.php
@@ -7,24 +7,37 @@ namespace App\Web\Blog;
 enum Author: string
 {
     case BRENT = 'brent';
+    case MARK = 'mark';
 
     public function getFullName(): string
     {
-        return match ($this) { self::BRENT => 'Brent Roose' };
+        return match ($this) {
+            self::BRENT => 'Brent Roose',
+            self::MARK => 'Márk Magyar',
+        };
     }
 
     public function getName(): string
     {
-        return match ($this) { self::BRENT => 'Brent' };
+        return match ($this) {
+            self::BRENT => 'Brent',
+            self::MARK => 'Márk',
+        };
     }
 
     public function getBluesky(): string
     {
-        return match ($this) { self::BRENT => 'brendt.bsky.social' };
+        return match ($this) {
+            self::BRENT => 'brendt.bsky.social',
+            default => null,
+        };
     }
 
     public function getX(): string
     {
-        return match ($this) { self::BRENT => 'brendt_gd' };
+        return match ($this) {
+            self::BRENT => 'brendt_gd',
+            default => null,
+        };
     }
 }
